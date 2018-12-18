cf66e290b0c97ff92be375082cb2e3c4b7c36d6c
mcguffin <joern@flyingletters.com>
2018-12-18 19:10:17 +0100

    Release version 0.0.7

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ Plugin Name: PDF Renderer
 Plugin URI: http://wordpress.org/
 Description: Enter description here.
 Author: Jörn Lund
-Version: 0.0.6
+Version: 0.0.7
 Author URI: https://github.com/mcguffin
 License: GPL3
 Github Repository: mcguffin/pdf-renderer
